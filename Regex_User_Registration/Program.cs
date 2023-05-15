ab8c698c1c78e01ee4c2a4f9510db77b35b080d8
Sushil <skshinde2028@gmail.com>
2023-05-15 23:57:16 +0530

    [Sushil] Adding welcome message

---
+++ Regex_User_Registration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_User_Registration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the validation of the User registration information by regex ");
+        }
+    }
+}
