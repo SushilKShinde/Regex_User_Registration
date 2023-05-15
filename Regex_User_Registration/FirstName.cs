03c53c8da9e462ad4ea7b27b4c0ed72ea218ec6e
Sushil <skshinde2028@gmail.com>
2023-05-16 00:38:25 +0530

    [Sushil] Validating first name

---
+++ Regex_User_Registration/FirstName.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Regex_User_Registration
+{
+    public class FirstName
+    {
+        public static void ValidateFirstName()
+        {
+            string pattern = "^[A-Z][a-zA-Z]*$";
+            string[] inputs = { "Sushil", "shWeta", "amiT", "Ojasss" };
+            Console.WriteLine("Inputs are: ");
+            PrintInputs(inputs);
+            Console.WriteLine();
+            ValidateInputs(inputs, pattern);
+        }
+
+        public static void PrintInputs(string[] inputs)
+        {
+            foreach (string input in inputs)
+            {
+                Console.Write($"{input} " );
+            }
+        }
+
+        public static void ValidateInputs(string[] inputs, string pattern)
+        {
+            foreach(string s in inputs)
+            {
+                if(Regex.IsMatch(s,pattern))
+                {
+                    Console.WriteLine($"Valid input: {s}");
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid Input: {s}");
+                }
+            }
+        }
+    }
+}
--- Regex_User_Registration/Program.cs
+++ Regex_User_Registration/Program.cs
@@ -10,7 +10,26 @@ namespace Regex_User_Registration
     {
         static void Main(string[] args)
         {
+            //Regex = Regular expression >>>>>>>Pattern matching or input validation
+            // ^ >>>>>> start of pattern
+            //[pattern to match ]
+            //{} >>>>> at least occurence 
+            //$ >>>>>> end of pattern
             Console.WriteLine("Welcome To the validation of the User registration information by regex ");
+            Console.WriteLine("Choose the correct option from the belo");
+            Console.WriteLine("1.Validate first name");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    FirstName.ValidateFirstName();
+                    break;
+                default:
+                    Console.WriteLine("Please enter valid input");
+                    break;
+            }
+
         }
     }
 }
